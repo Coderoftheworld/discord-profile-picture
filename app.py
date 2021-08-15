0e5dd796e3201708f70fc8f9827ca1a6e5d7f363
The Phantom Coder <forsem1234@gmail.com>
2021-08-15 16:28:25 -0400

    changed to server

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Web Page
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+# RESTful api endpoint
+@app.route("/scrape", methods=["POST"])
+def scrape():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
